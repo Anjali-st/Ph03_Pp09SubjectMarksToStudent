f9f35b8453c67ca612e4eafc8f5a9e55c7334df5
Anjali-st <anjali90908080@gmail.com>
2023-09-07 12:04:53 +0530

    RainBow School Section09

---
+++ Data/PracticePro_09DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppliPracticePro_09.Models;
+
+namespace WebAppliPracticePro_09.Data
+{
+    public class PracticePro_09DbContext : DbContext
+    {
+        public PracticePro_09DbContext (DbContextOptions<PracticePro_09DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppliPracticePro_09.Models.RainbowSchool> RainbowSchool { get; set; } = default!;
+    }
+}
